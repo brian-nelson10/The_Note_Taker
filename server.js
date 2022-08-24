aaf35b2934ac71425f6b9be6869d74026659faf3
brian <bn3l10@gmail.com>
2022-08-24 09:53:05 -0400

    created get route, works

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const apiRoutes = require('./Develop/routes/apiRoutes');
+//const htmlRoutes = require('./Develop/routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//parse incoming array or string data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming json data
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+//app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
