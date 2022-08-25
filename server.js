c6e5a263aa9ec646e9fed6863f9f77ed1e86cf14
brian <bn3l10@gmail.com>
2022-08-25 17:48:22 -0400

    fixed 404 when adding note

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path')
 const fs = require('fs');
-const apiRoutes = require('./routes/apiRoutes');
+//const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes/index1');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const note = require('./db/db.json');
+const theNote = require('./db/db.json');
 
 //parse incoming array or string data
 app.use(express.urlencoded({ extended: true }));
@@ -20,7 +20,7 @@ app.use(express.static('public'));
 // app.use('/', htmlRoutes);
 
 app.get('/api/notes', (req, res) => {
-  res.json(note.slice(1));
+  res.json(theNote.slice(1));
 })
 
 app.get('/', (req,res) => {
@@ -55,46 +55,18 @@ function createNewNote(body, notesArray) {
       return newNote;
 }
 
-// function filterByQuery(query, notesArray) {
-//   let filteredNotes = notesArray;
-//   if (query.title) {
-//       filteredNotes = filteredNotes.filter(note => note.title === query.title);
-//   }
-//   return filteredNotes;
-// }
+app.post('/api/notes', (req, res) => {
+  req.body.id = theNote.length.toString();
+
+      const newNote = createNewNote(req.body, theNote);
+      res.json(newNote);
+});
 
 function findById(id, notesArray) {
   const result = notesArray.filter(newNote => newNote.id === id)[0];
   return result;
 }
 
-
-function validateNote(newNote) {
-  if (!newNote.name || typeof newNote.name !== 'string') {
-      return false;
-  }
-  return true;
-}
-
-app.post("/notes", (req, res) => {
-  req.body.id = notes.length.toString();
-
-  if (!validateNote(req.body)) {
-      res.status(400).send("The Note is not properly formatted.");
-  } else {
-      const newNote = createNewNote(req.body, note);
-      res.json(newNote);
-  }
-});
-
-// app.get("/notes", (req, res) => {
-//   let results = notes;
-//   if (req.query) {
-//       results = filterByQuery(req.query, results);
-//   }
-//   console.log(req.query)
-//   res.json(results);
-// });
 
 app.get("/notes/:id", (req, res) => {
   const result = findById(req.params.id, notes);
